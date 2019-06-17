bda9b3fe71294c26ba02d18c4aaa9ac9aa0f8bf9
Joseph Kanner <josephkanner@gmail.com>
2019-06-17 11:30:01 +0300

    add task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+
+get 'list', to: 'tasks#list', as: :list
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
